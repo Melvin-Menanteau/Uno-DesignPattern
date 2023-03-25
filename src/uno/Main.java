29e3fe0d77010252bb5d55026b5c9e278d6ac519
benoitAD <benoit.augerdubois@gmail.com>
2023-03-25 20:20:59 +0100

    observer

--- src/uno/Main.java
+++ src/uno/Main.java
@@ -1,5 +1,12 @@
-public class Main {
+import joueurs.JoueurObserver;
+
+public class Main implements JoueurObserver {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    @Override
+    public void update() {
+
     }
 }
