97b4d95992abeef373007e4f2c481603ae7b68e6
Melvin MENANTEAU <melvin.menanteau@etu.univ-nantes.fr>
2023-03-27 10:32:17 +0200

    doc(Plateau)

--- src/uno/cartes/Plateau.java
+++ src/uno/cartes/Plateau.java
@@ -6,13 +6,20 @@ import uno.Partie;
 
 public class Plateau {
     private CarteBuilder carteBuilder;
+    // La pioche contenant les cartes encore disponible
     private ArrayList<Carte> pioche = new ArrayList<>();
+    // La dernière carte qui a été jouée
     private Carte carteCourante;
+    // La liste des cartes qui ont déjà été jouées.
+    // Permet de remplir la pioche une fois que cette dernière est vide
     private ArrayList<Carte> cartesJouees = new ArrayList<>();
 
     public Plateau(Partie partie) {
         carteBuilder = new CarteBuilder(partie);
 
+        // Création de l'ensemble des cartes du jeu
+
+        // Création des cartes ayant une couleur
         for (Carte.Couleur couleur : Carte.Couleur.values()) {
             if (couleur != Carte.Couleur.NOIR) {
                 for (int i = 0; i < 10; i++) {
@@ -20,6 +27,10 @@ public class Plateau {
                     carteBuilder.setCouleur(couleur);
                     carteBuilder.setValeur(i);
                     pioche.add(carteBuilder.build());
+
+                    // Les cartes ayant une couleur et n'étant pas 0 sont présente deux fois dans le jeu
+                    // il faut donc l'ajouter deux fois
+                    if (i != 0) pioche.add(carteBuilder.build());
                 }
 
                 for (Carte.Action action : Carte.Action.values()) {
@@ -30,10 +41,13 @@ public class Plateau {
                     carteBuilder.setCouleur(couleur);
                     carteBuilder.setComportement(action);
                     pioche.add(carteBuilder.build());
+                    // De la même manière, ajouter deux fois la carte
+                    pioche.add(carteBuilder.build());
                 }
             }
         }
 
+        // Création des cartes noires (plus4 / changement couleur)
         for (int i = 0; i < 4; i++) {
             carteBuilder.reset();
             carteBuilder.setComportement(Carte.Action.PLUS4);
@@ -44,18 +58,22 @@ public class Plateau {
             pioche.add(carteBuilder.build());
         }
 
+        // Mélanger la liste de cartes
         Collections.shuffle(pioche);
     }
 
+    // Changer la carte jouée, et ajouter la carte précedente à la liste de cartes déjà jouées
     public void setCarteCourante(Carte carte) {
         cartesJouees.add(carteCourante);
         carteCourante = carte;
     }
 
+    // Obtenir la dernière carte jouée, permet de vérifier que la carte que l'on souhaite jouée est valide
     public Carte getCarteCourante() {
         return carteCourante;
     }
 
+    // Permet de pioche une carte
     public Carte getCarte() {
         if (pioche.size() == 0) {
             pioche.addAll(cartesJouees);
@@ -64,10 +82,6 @@ public class Plateau {
         }
 
         return pioche.remove(0);
-    }
-
-    public ArrayList<Carte> getCartes() {
-        return pioche;
     }
 
     // use for testing
